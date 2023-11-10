2d14c9a5e5dee829f931b26e3d6eff73548ce35a
Ilya Lelkov <ilya.lelkov@mail.ru>
2023-11-10 13:39:46 +1000

    Completed!

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,29 +1,36 @@
 import styles from './Layout.module.css'
 import { Scoreboard } from '../ Scoreboard/ Scoreboard.jsx'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Board } from '../Board/Board.jsx'
 import { calculation } from '../../calculation.js'
 import { store } from '../../store.js'
 
 export const Layout = () => {
-	// const [board, setBoard] = useState(Array(9).fill(''))
 	const [xIsNext, setXIsNext] = useState(true)
 
 	const board = store.getState()
 
 	const winner = calculation(board)
 
+	useEffect(() => {
+		const unsubscribe = store.subscribe(() => {
+			setXIsNext(!xIsNext)
+		})
+
+		return () => {
+			unsubscribe()
+		}
+	}, [xIsNext])
+
 	const handleClick = (index) => {
 		const boardCopy = [...board]
 		if (winner || boardCopy[index]) return
 		boardCopy[index] = xIsNext ? 'X' : 'O'
-		// setBoard(boardCopy)
 		store.dispatch({ type: 'CURRENT_BOARD', payload: boardCopy })
 		setXIsNext(!xIsNext)
 	}
 
 	const newGame = () => {
-		// setBoard(Array(9).fill(''))
 		store.dispatch({ type: 'NEW_GAME' })
 	}
 
--- src/store.js
+++ src/store.js
@@ -2,12 +2,24 @@ import { appReducer } from './reducer.js'
 
 const createStore = (reducer) => {
 	let state
+	const listeners = []
 
 	return {
 		dispatch: (action) => {
 			state = reducer(state, action)
+			listeners.forEach((listener) => listener(state))
 		},
-		getState: () => state
+		getState: () => state,
+		subscribe: (listener) => {
+			listeners.push(listener)
+
+			return () => {
+				const index = listeners.indexOf(listener)
+				if (index !== -1) {
+					listeners.splice(index, 1)
+				}
+			}
+		}
 	}
 }
 
